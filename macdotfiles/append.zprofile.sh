7c6427ab638afa51a5820dc8caa6d6f5867c414f
David Acevedo <rebelwarrior@users.noreply.github.com>
2021-07-11 08:52:05 -0400

    Update append.zprofile.sh

--- macdotfiles/append.zprofile.sh
+++ macdotfiles/append.zprofile.sh
@@ -1,5 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+if (type pyenv >/dev/null 2>&1 || exit 1 ); then eval "$(pyenv init --path)"; fi
 
-export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+# export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+eval "$(/opt/homebrew/bin/brew shellenv)"
