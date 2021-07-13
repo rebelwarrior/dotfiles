c397b1e30a4121864cff49fd67aa28f3babb8e1e
David Acevedo <rebelwarrior@users.noreply.github.com>
2021-07-12 22:18:58 -0400

    Update append.zprofile.sh

--- macdotfiles/append.zprofile.sh
+++ macdotfiles/append.zprofile.sh
@@ -1,6 +1,7 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-if (type pyenv >/dev/null 2>&1 || exit 1 ); then eval "$(pyenv init --path)"; fi
+# eval "$(pyenv init --path)" This returns the below line and executing it. 
+export PATH="$HOME/.pyenv/shims:${PATH}"
 
 # export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
 eval "$(/opt/homebrew/bin/brew shellenv)"
